ef997934bc8fa4e5721e46782b27e9cb5997d65d
wurusai <dinggongwurusai@163.com>
2022-07-19 21:36:00 +0800

    push commit

--- src/main/java/com/wurusai/git/GitTest.java
+++ src/main/java/com/wurusai/git/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!!!");
         System.out.println("Hello Git2!!!");
+        System.out.println("Hello Git3!!!");
+        System.out.println("push test!!!");
     }
 }
